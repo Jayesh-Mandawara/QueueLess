e317952220246f851bdd8f3295a4efa8c7e6c82c
Jayesh Mandawara <jaymandawara08@gmail.com>
2026-09-14 22:38:23 +0530

    Connect mobile app to Render backend

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -1,25 +1,25 @@
-import axios from 'axios';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 // Replace with your machine's local IP or localhost (10.0.2.2 for Android emulator)
-export const BASE_URL = 'http://127.0.0.1:8000/api';
+export const BASE_URL = "https://queueless-hz3s.onrender.com/api";
 
 const api = axios.create({
-  baseURL: BASE_URL,
-  headers: {
-    'Content-Type': 'application/json',
-  },
+    baseURL: BASE_URL,
+    headers: {
+        "Content-Type": "application/json",
+    },
 });
 
 api.interceptors.request.use(
-  async (config) => {
-    const token = await AsyncStorage.getItem('access_token');
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
+    async (config) => {
+        const token = await AsyncStorage.getItem("access_token");
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => Promise.reject(error),
 );
 
 export default api;
